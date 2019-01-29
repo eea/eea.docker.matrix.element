13b13f9a1b4bc0f6ff4444f2762843851e242144
valentinab25 <valentinab25>
2019-01-29 15:31:37 +0200

    Add new configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,21 +10,40 @@ if [ -z "${IDENTITY_SERVER_URL}" ]; then
 	export IDENTITY_SERVER_URL="http://localhost:8090"
 fi
 
-if [ -z "${BRAND}" ]; then
-	echo "BRAND not set, 'Riot' by default"
-	export BRAND="Riot"
-fi
 
 generate_config() {
 cat <<EOF > /var/www/riot/config.json
 {
     "default_hs_url": "${HOME_SERVER_URL}",
     "default_is_url": "${IDENTITY_SERVER_URL}",
-    "brand": "${BRAND}",
+    "disable_custom_urls": false,
+    "disable_guests": false,
+    "disable_login_language_selector": false,
+    "disable_3pid_login": false,
+    "brand": "Riot",
     "integrations_ui_url": "https://scalar.vector.im/",
-    "integrations_rest_url": "https://scalar.vector.im/api"
-
+    "integrations_rest_url": "https://scalar.vector.im/api",
+    "integrations_jitsi_widget_url": "https://scalar.vector.im/api/widgets/jitsi.html",
+    "bug_report_endpoint_url": "https://riot.im/bugreports/submit",
+    "features": {
+        "feature_groups": "labs",
+        "feature_pinning": "labs"
+    },
+    "default_federate": true,
+    "welcomePageUrl": "home.html",
+    "default_theme": "dark",
+    "roomDirectory": {
+        "servers": [
+            "${HOME_SERVER_URL}"
+        ]
+    },
+    "welcomeUserId": "@riot-bot:matrix.org",
+    "piwik": false,
+    "enable_presence_by_hs_url": {
+        "${HOME_SERVER_URL}": false
+    }
 }
+
 EOF
 }
 
